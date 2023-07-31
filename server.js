6161d46e31858df505aeb4e3b39d5975f52b13a3
Adesewa Aishat <adesewaaishat@gmail.com>
2023-07-31 23:33:44 +0100

    add book endpoint

--- server.js
+++ server.js
@@ -1,18 +1,30 @@
-'use strict';
+"use strict";
 
-require('dotenv').config();
-const express = require('express');
-const cors = require('cors');
+const mongoose = require("mongoose");
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const Book = require("./db/schema/book");
+
+mongoose
+  .connect(process.env.DATABASE_URL)
+  .then(() => console.log("Connected!"))
+  .catch((error) => {
+    console.log(error);
+  });
 
 const app = express();
 app.use(cors());
 
 const PORT = process.env.PORT || 3001;
 
-app.get('/test', (request, response) => {
+app.get("/", (request, response) => {
+  response.send("hello");
+});
 
-  response.send('test request received')
-
-})
+app.get("/book", async (request, response) => {
+  const books = await Book.find();
+  response.send(books);
+});
 
 app.listen(PORT, () => console.log(`listening on ${PORT}`));
